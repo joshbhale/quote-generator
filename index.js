10924a1b9e2105d5dfec7fa6b2dd9d89b6199df8
joshbhale <joshbhale@gmail.com>
2024-10-26 17:52:55 -0400

    const removed from arrays

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ server.listen(port, hostname, () => {
 
 function randomQuote(inspQuote, philQuote, funFact) {
 
-  const inspQuote = ["Be yourself; everyone else is already taken - Oscar Wilde", "It is during our darkest moments that we must focus to see the light - Aristotle",
+  inspQuote = ["Be yourself; everyone else is already taken - Oscar Wilde", "It is during our darkest moments that we must focus to see the light - Aristotle",
       "If opportunity doesn't knock, build a door -Milton Berle", 
       "Perfection is not attainable, but if we chase perfection we can catch excellence -Vince Lombardi",
       "A champion is someone who gets up when he can't -Jack Dempsey",
       "It is never too late to be what you might have been -George Eliot"]
-  const philQuote = ["Most people are other people. Their thoughts are someone else's opinions, their lives a mimicry, their passions a quotation -Oscar Wilde",
+  philQuote = ["Most people are other people. Their thoughts are someone else's opinions, their lives a mimicry, their passions a quotation -Oscar Wilde",
       "That which can be destroyed by the truth should be -P.C. Hodgell",
       "Once upon a time, in some out of the way corner of that universe which is dispersed into numberless twinkling solar systems, there was a star upon which clever beasts invented knowing. That was the most arrogant and mendacious minute of world history, but nevertheless, it was only a minute. After nature had drawn a few breaths, the star cooled and congealed, and the clever beasts had to die. One might invent such a fable, and yet he still would not have adequately illustrated how miserable, how shadowy and transient, how aimless and arbitrary the human intellect looks within nature -Friedrich Nietzsche",
       "Death does not concern us, because as long as we exist, death is not here. And when it does come, we no longer exist -Epicurus",
       "It is the mark of an educated mind to be able to entertain an idea without accepting it -Aristotle"]
-  const funFact = ["Tigers have striped skin", "Octopi have three hearts", "Earth's rotation is slowing down",
+  funFact = ["Tigers have striped skin", "Octopi have three hearts", "Earth's rotation is slowing down",
       "The universe's average color is called Cosmic Latte", "The fear of long words is called Hippopotomonstrosesquippedaliophobia"]
 
     function randomNumber() {
